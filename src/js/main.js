af3697fc8945f9c482521e07332cfc8bd8b44698
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2024-10-14 13:24:40 +0000

    rendering pokemon name as p

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,24 @@
 import data from "./data.json";
 
-const inputEl = document.querySelector("#floatingInputGroup1");
-console.log(inputEl);
+// === DOM Selection
+const inputEl = document.querySelector("input");
+const pokemonRow = document.querySelector("[data-pokemon-row]");
 
-inputEl.focus();
+// Iterate over pokemon data
+for (let obj of data) {
+  const { name } = obj; // destructure
+  // Step1: Create a paragraph
+  const paragraph = document.createElement("p");
+  // Step2: Paragraph ka content = pokemon ka name
+  paragraph.textContent = name;
+  // Step3: Paragraph ko pokemonRow mein add karana hai
+  pokemonRow.appendChild(paragraph);
+}
 
-// for (let object of data) {
-//   console.log(object.name);
-// }
+// === Add keyboard functionality
+document.addEventListener("keyup", (event) => {
+  if (event.key === "/") {
+    console.log(`Slash was pressed`);
+    inputEl.focus();
+  }
+});
