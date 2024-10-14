6d739345b74d836db3639053dda8f18dcafb26f0
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2024-10-14 14:51:34 +0000

    Add PokemonCard component

---
+++ src/js/components/PokemonCard.js
@@ -0,0 +1,22 @@
+// A part of UI = Component
+// A component can be made using other components (composable)
+export default function (name, image, description, link) {
+  const div = document.createElement("div");
+  div.classList.add("col");
+  div.innerHTML = `
+      <div class="card">
+      <img
+        src="${image}"
+        class="card-img-top"
+        alt="${name}"
+      />
+      <div class="card-body">
+        <h5 class="card-title">${name}</h5>
+        <p class="card-text">${description}</p>
+        <a href="${link}" class="btn btn-warning" target="_blank">Visit</a>
+      </div>
+    </div>
+    `;
+
+  return div;
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 import data from "./data.json";
+import PokemonCard from "./components/PokemonCard";
 
 // === DOM Selection
 const inputEl = document.querySelector("input");
@@ -6,34 +7,8 @@ const pokemonRow = document.querySelector("[data-pokemon-row]");
 
 // Iterate over pokemon data
 for (let obj of data) {
-  const { name, image, description } = obj; // destructure
-
-  // Step1: Create a paragraph
-  const paragraph = document.createElement("p");
-  // Step2: Paragraph ka content = pokemon ka name
-  paragraph.textContent = name;
-  // Step3: Paragraph ko pokemonRow mein add karana hai
-  pokemonRow.appendChild(pokemonCard(name, image, description));
-}
-
-function pokemonCard(name, image, description) {
-  const div = document.createElement("div");
-  div.classList.add("col");
-  div.innerHTML = `
-    <div class="card">
-    <img
-      src="${image}"
-      class="card-img-top"
-      alt="${name}"
-    />
-    <div class="card-body">
-      <h5 class="card-title">${name}</h5>
-      <p class="card-text">${description}</p>
-    </div>
-  </div>
-  `;
-
-  return div;
+  const { name, image, description, link } = obj; // destructure
+  pokemonRow.appendChild(PokemonCard(name, image, description, link));
 }
 
 // === Add keyboard functionality
